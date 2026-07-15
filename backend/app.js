3df58449f9baea81a7dc49d7ffa3015d989b3450
ayuushpokharel <ayuushpokharel@gmail.com>
2026-07-15 23:33:38 +0545

    render deployment setup

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,14 @@ const path = require("path");
 const cors = require("cors");
 const errorMiddleware = require("./middleware/error");
 
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  });
+}
+
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
